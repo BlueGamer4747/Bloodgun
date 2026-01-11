55266e4baf5860be6a256047f4c8dfe0cb1df717
JhannerAlmeida <129618808+JhannerAlmeida@users.noreply.github.com>
2026-01-11 15:26:30 -0300

    pequenos ajustes

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
-const SPEED = 8.0
-const JUMP_VELOCITY = 4.5
-
 @export var TILT_LOWER_LIMIT := deg_to_rad(-90)
+@export var JUMP_VELOCITY : float = 4.5
+@export var SPEED : float = 9.0
 @export var TILT_UPPER_LIMIT := deg_to_rad(90)
 @export var CAMERA_CONTROLLER : Node3D
-@export var MOUSE_SENTIVITY : float = 0.3
+@export var MOUSE_SENTIVITY : float = 0.5
+@export var ANIMATIONPLAYER : AnimationPlayer
+@export_range(5, 10, 0.1) var CORUCH_SPEED : float = 8.0
+@export var CROUCH_SHAPECAST : Node3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var _mouse_rotation : Vector3
@@ -16,6 +18,18 @@ var _tilt_input : float
 var _player_rotation : Vector3
 var _camera_rotation : Vector3
 var _is_crouching : bool = false
+
+func _on_animation_player_animation_started(anim_name: StringName) -> void:
+	if anim_name == "CROUCH":
+		_is_crouching = !_is_crouching
+
+func _toggle_crouch():
+	if _is_crouching == true and CROUCH_SHAPECAST.is_colliding() == false:
+		ANIMATIONPLAYER.play("CROUCH", -1, -CORUCH_SPEED, true)
+		print("UNCROUCH")
+	elif _is_crouching == false:
+		ANIMATIONPLAYER.play("CROUCH", -1, CORUCH_SPEED)
+		print("CROUCH")
 
 func _update_camera(delta):
 	_mouse_rotation.x += _tilt_input * delta
@@ -39,9 +53,14 @@ func _unhandled_input(event: InputEvent) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
+	if event.is_action_pressed("crouch") and is_on_floor():
+		_toggle_crouch()
+	if event.is_action_pressed("reload"):
+		get_tree().reload_current_scene()
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	CROUCH_SHAPECAST.add_exception($".")
 
 func _physics_process(delta: float) -> void:
 	
@@ -49,7 +68,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	_update_camera(delta)
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and _is_crouching == false:
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
